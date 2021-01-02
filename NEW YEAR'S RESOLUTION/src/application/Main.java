03d5a8ac7827ebf4194998924c2981d92e0bc5c4
woogieReal <75457703+woogieReal@users.noreply.github.com>
2021-01-02 18:02:05 +0900

    New Year Resolution_2
    1. Making a main page

--- NEW YEAR'S RESOLUTION/src/application/Main.java
+++ NEW YEAR'S RESOLUTION/src/application/Main.java
@@ -1,21 +1,24 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		Parent root;
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			root = FXMLLoader.load(getClass().getResource("FxDesign.fxml"));
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
