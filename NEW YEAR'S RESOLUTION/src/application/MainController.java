d51065270eba2c71222380300fba74847ad4e245
woogieReal <75457703+woogieReal@users.noreply.github.com>
2021-01-07 20:47:58 +0900

    New Year's Resolution_13
    1. if user didn't check the complete button yesterday, data is changed from '미완료(incomplete)' to '종료(end)'. Also incomplete count is increased.

--- NEW YEAR'S RESOLUTION/src/application/MainController.java
+++ NEW YEAR'S RESOLUTION/src/application/MainController.java
@@ -3,8 +3,10 @@ package application;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -168,7 +170,44 @@ public class MainController {
 		//주간 페이지
 		WeeklyController.WeeklyTable(dateInWeeklyLabel, viewInWeeklyTabel, noInWeeklyColumn ,monColumn, tueColumn, wedColumn, thuColumn, friColumn, satColumn, sunColumn);
 		
-
+		//incomplete 처리
+		String yesterday = LocalDate.now().minusDays(1).toString();
+		String[] str2 = yesterday.split("-");
+		String result2 = str2[0]+"y"+str2[1]+"m"+str2[2]+"d";
+		String SQL = "SELECT complete FROM "+ result2;
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("zero");
+		try {
+			rs = st.executeQuery(SQL);
+			while(rs.next()) {
+				arr.add(rs.getString(1));
+			}
+			for(int i = 1; i < arr.size(); i++) {
+				if(arr.get(i).equals("미완료")) {
+					
+					String SQL2 = "UPDATE "+ result2 +" SET complete = '종료' WHERE num = "+ i;
+					st.executeUpdate(SQL2);
+					
+					String SQL3 = "SELECT incomplete FROM all_goals WHERE no = "+i;
+					rs = st.executeQuery(SQL3);
+					int newIncomplete = 0;
+					while(rs.next()) {
+						newIncomplete = Integer.parseInt(rs.getString(1)) + 1;
+					}
+					
+					String SQL4 = "UPDATE all_goals SET incomplete = " + newIncomplete + " WHERE no = "+i;
+					st.executeUpdate(SQL4);
+					System.out.println("update 종료 성공");
+					updateTable();
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("incomplete 처리실패: "+e.getMessage());
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 	
 	public void updateTable() {
